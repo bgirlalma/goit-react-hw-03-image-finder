ca5c76acc4cff493e8279f40f1329b59465ea9b5
Alina Akulina <124092247+bgirlalma@users.noreply.github.com>
2023-08-17 18:53:09 +0300

    ++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { GlobalStyled } from "./global.styled";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      
+      <GlobalStyled/>
     </div>
   );
 };
